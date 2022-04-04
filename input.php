174a34ad30c32c94b833b9455ddefe2d025ef7a5
FatimaKiran <fmian7860@gmail.com>
2022-04-04 11:39:01 +0100

    taking user input

--- input.php
+++ input.php
@@ -1,28 +1,28 @@
 
 <?php
-   
-// Input section
-// $a = 20
-// $a = (int)readline('Enter an integer: ');
-  
-// // $b = 20.12
-// $b = (float)readline('Enter a floating'
-//             . ' point number: ');
-  
-// // Entered integer is 20 and
-// // entered float is 20.12
-// echo "Entered integer is " . $a 
-//     . " and entered float is " . $b;
 
-$a = readline('Player1 Name: ');
-
-echo $a .  " ";
-
-$b = readline("Please select any pokemon : \n a \n b \n c\n type......  ");
+$pokemons = ["Bulbasaur", "RoseRade" , "Squirtle" , "Leafeon",  "Osha wott","Gyarados" , "Ho-Oh",  "Vulpix" , "Infernape"];
+$statment = "Please Select any pokemon 😃: \n Bulbasaur\n RoseRade \n Leafeon \n Squirtle \n Osha wott \n Gyarados \n Ho-Oh \n Vulpix \n Infernape\n\nType Below your favourite pokemon name: \n";
 
 
+$player1 = readline('Player1 Name: ');
+$pokemon1 = readline( $statment );
+while (!in_array($pokemon1, $pokemons)) {
+    echo "Please select any of the above pokemon\n";
+    $pokemon1 = readline("Type here: ");
+}
+echo "\n";
 
+$player2 = readline('Player2 Name: ');
+$pokemon2 = readline($statment);
+while (!in_array($pokemon2, $pokemons)) {
+    echo "Please select any of the above pokemon\n";
+    $pokemon2 = readline("Type here: ");
+}
 
+echo "\n\t\t\t\t\t....................... POKEMON BATTLE BEGINS ..........................\n";
 
-?>
+echo " \t\t\t\t\t\t\t\t  '".strtoupper($pokemon1). "'" ." VS " . "'".  strtoupper($pokemon2)."' \n"  ;
+?> 
+
 
