b00461ff1c46d375e6972e77333ecf7a89d5a9ea
FatimaKiran <fmian7860@gmail.com>
2022-04-04 11:38:03 +0100

    pokemons data

---
+++ pokemons.php
@@ -0,0 +1,14 @@
+<?php
+    $pokemons = [
+        "Bulbasaur" => ["Bulbasaur","30","180","bar","solar beam","grass"],
+        "RoseRade"  => ["RoseRade","20","50","hoo","poisonjab","grass"],
+        "Squirtle"  => ["Squirtle","40","45","foo","aqua jet","water"],
+        "Leafeon"   => ["Leafeon","50","70","meow","quick attack","grass"],
+        "Osha wott" => ["Osha wott","90","50","foo","water gun","water"],
+        "Gyarados"  => ["Gyarados","100","170","bar","evolves from","water"],
+        "Ho-Oh"     => ["Ho-Oh","120","140","meow","fire blast","fire"],
+        "Vulpix"    => ["Vulpix" , "120","60","bar","fireblast","fire"],
+        "Infernape"=> ["Infernape","100","70","hoo","fire spin","fire"]
+    ];
+
+?>
